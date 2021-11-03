cba0ba5c61e1dad81508e78166bb33f159e58b91
DESKTOP-96ROCF3\m4kr <jimotme030@naver.com>
2021-11-03 15:39:28 +0900

    Update Master Branch

---
+++ 1101/Assets/TestScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("테스트용 스크립트입니다");
+        Debug.Log("테스트용 스크립트입니다");
+        Debug.Log("테스트용 스크립트입니다");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
