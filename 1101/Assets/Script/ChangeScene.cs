f0266691682f5236803e63e3e8a66be3ce005876
hoyeols <ghduf6073@naver.com>
2021-11-09 16:22:17 +0900

    [#211109] feature/hoyeol : 아바타정보입력-남자버튼과 여자버튼 다르게 생성하기

--- 1101/Assets/Script/ChangeScene.cs
+++ 1101/Assets/Script/ChangeScene.cs
@@ -7,21 +7,33 @@ using UnityEngine.EventSystems;
 public class ChangeScene : MonoBehaviour
 {
     public string nowButton;
+    public string genderButton;
+
     // Start is called before the first frame update
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
+
     }
 
+    public void ButtonClick()
+    {
+        // 현재 게임오브젝트의 이름을 저장한다
+        genderButton = EventSystem.current.currentSelectedGameObject.name;
+        if (genderButton == "Male")
+            Debug.Log("남자");
+        else if (genderButton == "Female")
+            Debug.Log("여자");
+    }
     // Update is called once per frame
     public void SceneChange()
     {
-        // 현재 게임오브젝트의 이름을 저장한다
         nowButton = EventSystem.current.currentSelectedGameObject.name;
-        /*if (nowButton == "CreateAvatar")
-            Debug.Log("같다");
-        else
-            Debug.Log("다르다");*/
-        SceneManager.LoadScene("SampleScene");
+        if (nowButton == "CreateAvatar")
+        {
+            Debug.Log("아바타생성");
+            SceneManager.LoadScene("SampleScene");
+        }
     }
+
 }
--- 1101/Assets/Script/CreateAvatar.cs
+++ 1101/Assets/Script/CreateAvatar.cs
@@ -8,27 +8,42 @@ public class CreateAvatar : MonoBehaviour
     string      avatarGender;
     GameObject  avatarManager;
     public GameObject  avatar;
+
     // Start is called before the first frame update
     void Start()
     {
         //다른 스크립트 파일의 변수 접근 법
         // GameObject.Find("다른 스크립트 파일이 들어있는 오브젝트")
         avatarManager = GameObject.Find("GameObject");
-        avatarGender = avatarManager.GetComponent<ChangeScene>().nowButton;
+        avatarGender = avatarManager.GetComponent<ChangeScene>().genderButton;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (avatarGender == "CreateAvatar")
+        if (avatarGender == "Male")
         {
-            Debug.Log("성공");
+            Debug.Log("여자 캐릭터를 가린다");
             avatar.SetActive(false);
         }
-        else
+        else if (avatarGender == "Female")
         {
-            Debug.Log("실패");
+            Debug.Log("남자 캐릭터를 가린다");
             avatar.SetActive(true);
         }
+
     }
+/*
+   void PressBtnB() {
+
+	// B 버튼 눌렀을 때 A버튼의 onClick 이벤트 끊기.
+	btnA.onClick.RemoveListener(DoSomething) ;
+
+    }
+    void PressBtnB() {
+
+	// B 버튼 눌렀을 때 A버튼의 onClick 이벤트 끊기.
+	btnA.onClick.RemoveListener(DoSomething) ;
+
+    }*/
 }
