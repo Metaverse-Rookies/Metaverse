d244ca4cda41b617b768e7acf672900bb575e300
hoyeols <ghduf6073@naver.com>
2021-12-08 16:40:01 +0900

    [#211208] feature/hoyeol : TV 추천창 : 동적 하이퍼링크 달기

--- 211123/Assets/Assets_Recommend_hoyeol/UI/FancyScrollView/Examples/Sources/09_LoadTexture/Cell.cs
+++ 211123/Assets/Assets_Recommend_hoyeol/UI/FancyScrollView/Examples/Sources/09_LoadTexture/Cell.cs
@@ -22,7 +22,7 @@ namespace FancyScrollView.Example09
         [SerializeField] CanvasGroup canvasGroup = default;
 
         ItemData data;
-
+        Button btn;
         public override void UpdateContent(ItemData itemData)
         {
             data = itemData;
@@ -83,6 +83,44 @@ namespace FancyScrollView.Example09
             canvasGroup.alpha = alphaEasing(1f - slide);
 
             background.color = Color.Lerp(Color.gray, Color.white, pop);
+        }
+        
+        void Start()
+        {
+            btn = gameObject.AddComponent<Button>();
+            Debug.Log(title.text);
+            if (title.text == "상품번호 S0167913")
+                btn.onClick.AddListener(AnamURL);
+            else if (title.text == "상품번호 S2974243")
+                btn.onClick.AddListener(Samsung75URL);
+            else if (title.text == "상품번호 S2987021")
+                btn.onClick.AddListener(Samsung85URL);
+            else if (title.text == "상품번호 S3121113")
+                btn.onClick.AddListener(Lg48URL);
+            else if (title.text == "상품번호 S3308023")
+                btn.onClick.AddListener(Lg77URL);
+            else
+                btn.onClick.AddListener(AnamURL);
+        }
+        public void AnamURL()
+        {
+            Application.OpenURL("https://m.etlandmall.co.kr/mobile/product/product.do?prdMstCd=S0167913");
+        }
+        public void Lg48URL()
+        {
+            Application.OpenURL("https://m.etlandmall.co.kr/mobile/product/product.do?prdMstCd=S3121113");
+        }
+        public void Samsung85URL()
+        {
+            Application.OpenURL("https://m.etlandmall.co.kr/mobile/product/product.do?prdMstCd=S2987021");
+        }
+        public void Samsung75URL()
+        {
+            Application.OpenURL("https://m.etlandmall.co.kr/mobile/product/product.do?prdMstCd=S2974243");
+        }
+        public void Lg77URL()
+        {
+            Application.OpenURL("https://m.etlandmall.co.kr/mobile/product/product.do?prdMstCd=S3308023");
         }
     }
 }
---
+++ 211123/Assets/Script/OpenURL.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour
+{
+    public void AnamURL()
+    {
+        Application.OpenURL("https://m.etlandmall.co.kr/mobile/product/product.do?prdMstCd=S0167913");
+    }
+    public void Lg48URL()
+    {
+        Application.OpenURL("https://m.etlandmall.co.kr/mobile/product/product.do?prdMstCd=S3121113");
+    }
+    public void Samsung85URL()
+    {
+        Application.OpenURL("https://m.etlandmall.co.kr/mobile/product/product.do?prdMstCd=S2987021");
+    }
+    public void Samsung75URL()
+    {
+        Application.OpenURL("https://m.etlandmall.co.kr/mobile/product/product.do?prdMstCd=S2974243");
+    }
+    public void Lg77URL()
+    {
+        Application.OpenURL("https://m.etlandmall.co.kr/mobile/product/product.do?prdMstCd=S3308023");
+    }
+}
