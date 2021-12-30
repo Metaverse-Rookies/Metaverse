0d0d611a7378876bb0a80b0d661ff6a551419af8
Yunju Jang <rlo3olr@naver.com>
2021-12-30 13:01:39 +0900

    #211230 윤주 - StartPoint, Door Animation

--- 211228 호열/Assets/Script/Login/CreateAvatar.cs
+++ 211228 호열/Assets/Script/Login/CreateAvatar.cs
@@ -14,35 +14,6 @@ public class CreateAvatar : MonoBehaviour
     public string prevScene;
     public string thisScene;
 
-    /*
-    void Awake()
-    {
-        //다른 스크립트 파일의 변수 접근 법
-        // GameObject.Find("다른 스크립트 파일이 들어있는 오브젝트")
-        avatarManager = GameObject.Find("GameObject");
-        avatarGender = avatarManager.GetComponent<ChangeScene>().genderButton;
-        avatarName = avatarManager.GetComponent<ChangeScene>().name;
-        Debug.Log(avatarName);
-        GameObject childName = transform.Find("Name").gameObject;
-        childName.GetComponent<TextMesh>().text = avatarName;
-        childName.GetComponent<TextMesh>().characterSize = .5f;
-    }
-
-    // Update is called once per frame
-    void Start()
-    {
-        if (avatarGender == "Male")
-        {
-            Debug.Log("여자 캐릭터를 가린다");
-            avatar.SetActive(false);
-        }
-        else if (avatarGender == "Female")
-        {
-            Debug.Log("남자 캐릭터를 가린다");
-            avatar.SetActive(true);
-        }
-
-    }*/
     
     void Update(){
         // 씬 전환
@@ -61,6 +32,12 @@ public class CreateAvatar : MonoBehaviour
             {
                 GameObject target = GameObject.Find("TargetPosition2");
                 this.transform.position = target.transform.position;
+            }
+            else {
+                if(thisScene != "LoginScene"){
+                    GameObject target = GameObject.Find("StartPoint");
+                    this.transform.position = target.transform.position;
+                }
             }
         }
     }
