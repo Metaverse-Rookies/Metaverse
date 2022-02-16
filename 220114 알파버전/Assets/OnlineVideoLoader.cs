54725ca08ab435bc2dcf735ab6bf9a8c1216c2fc
hoyeols <ghduf6073@naver.com>
2022-02-16 11:02:56 +0900

    비디오 url 스크립트 수정

--- 220114 알파버전/Assets/OnlineVideoLoader.cs
+++ 220114 알파버전/Assets/OnlineVideoLoader.cs
@@ -5,14 +5,35 @@ using UnityEngine.Video;
  
 public class OnlineVideoLoader : MonoBehaviour
 {
-     
+    public GameObject _Channels; 
     public VideoPlayer videoPlayer;
-    public string videoUrl = "https://ktds-rookies-metaverse.s3.ap-northeast-2.amazonaws.com/Video/Drama.mp4";
-     
+    public string videoCommonUrl = "https://ktds-rookies-metaverse.s3.ap-northeast-2.amazonaws.com/Video/";
+    private string videoUrl;
+    private MeshRenderer MeshRendererMode;
     // Start is called before the first frame update
     void Start()
     {
-        videoPlayer.url = videoUrl;
+        GameObject tempObject;
+
+        videoUrl = "Animation.mp4";
+        if (videoUrl.Contains("Drama"))
+        {
+            tempObject = _Channels.transform.GetChild(2).gameObject;
+            MeshRendererMode = tempObject.GetComponent<MeshRenderer>();
+        }
+        else if (videoUrl.Contains("Document"))
+        {
+            tempObject = _Channels.transform.GetChild(0).gameObject;
+            MeshRendererMode = tempObject.GetComponent<MeshRenderer>();
+        }
+        else
+        {
+            tempObject = _Channels.transform.GetChild(1).gameObject;
+            MeshRendererMode = tempObject.GetComponent<MeshRenderer>();
+        }
+        
+        videoPlayer.targetMaterialRenderer = MeshRendererMode;
+        videoPlayer.url = videoCommonUrl + videoUrl;
         videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
         videoPlayer.EnableAudioTrack (0, true);
         videoPlayer.Prepare ();
